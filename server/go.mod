eddae9e711cbaa586736cf96b16a8337afd4146f
JeffC25 <34695547+JeffC25@users.noreply.github.com>
2023-08-12 11:00:32 +0800

    update config and logs

--- server/go.mod
+++ server/go.mod
@@ -4,5 +4,12 @@ go 1.20
 
 require (
 	github.com/go-chi/chi/v5 v5.0.10
+	github.com/rs/zerolog v1.30.0
 	gopkg.in/yaml.v2 v2.4.0
+)
+
+require (
+	github.com/mattn/go-colorable v0.1.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	golang.org/x/sys v0.0.0-20210927094055-39ccf1dd6fa6 // indirect
 )
---
+++ server/log/logger.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"io"
+	"os"
+	"time"
+
+	"github.com/rs/zerolog"
+)
+
+func Logger(level int) zerolog.Logger {
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnixMs
+
+	var output io.Writer
+
+	output = zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: time.RFC3339}
+
+	return zerolog.New(output).With().Caller().Timestamp().Logger().Level(zerolog.Level(level))
+}
