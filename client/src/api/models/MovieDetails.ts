3d0d4028a485fd5ba60abb050c49c310474e6966
JeffC25 <34695547+JeffC25@users.noreply.github.com>
2023-08-24 00:21:08 -0400

    setup openapi on frontend

---
+++ client/src/api/models/MovieDetails.ts
@@ -0,0 +1,15 @@
+/* generated using openapi-typescript-codegen -- do no edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+export type MovieDetails = {
+    poster: string;
+    name: string;
+    rating: number;
+    date: string;
+    overview: string;
+    homepage: string;
+    backdrop: string;
+};
+
---
+++ client/src/api/models/Video.ts
@@ -0,0 +1,11 @@
+/* generated using openapi-typescript-codegen -- do no edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+export type Video = {
+    title: string;
+    link: string;
+    trailer: boolean;
+};
+
