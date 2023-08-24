0a84eb377e5c65c580dfe53edebe91eb7d6f3b65
JeffC25 <34695547+JeffC25@users.noreply.github.com>
2023-08-24 03:37:15 -0400

    set up tailwind

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Route, Routes} from 'react-router-dom'
+import NavBar from './components/NavBar'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <BrowserRouter>
+      <NavBar/>
+      <div className="flex bg-gray-700 min-h-screen">
+        <Routes>
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </BrowserRouter>
   )
 }
 
---
+++ client/src/components/NavBar.tsx
@@ -0,0 +1,9 @@
+const NavBar = () => {
+    return (
+        <nav className="relative bg-gray-900 w-full h-16 shadow">
+            <div className=" flex justify-center items-center "></div>
+        </nav>
+      )
+}
+
+export default NavBar
