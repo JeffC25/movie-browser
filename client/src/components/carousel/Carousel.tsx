8d55a179e1940bb591d78f454a731c5647cd5f0f
JeffC25 <34695547+JeffC25@users.noreply.github.com>
2023-08-24 23:41:17 -0400

    change right margin in carousel to tailwind space-between

--- client/src/components/carousel/Carousel.tsx
+++ client/src/components/carousel/Carousel.tsx
@@ -10,7 +10,7 @@ const Carousel = ( {title, slides }: Props) => {
     return (
         <div className="block w-4/5 mx-auto my-10">
             <div className="text-left text-white mb-1">{title}</div>
-            <div className="overflow-x-scroll w-full h-72 bg-gray-50 rounded-lg mx-auto border-2 flex ">
+            <div className="overflow-x-scroll w-full h-72 bg-gray-50 rounded-lg mx-auto border-2 flex space-x-1">
                 {...slides}
             </div>
         </div>
--- client/src/components/movies/MoviePreview.tsx
+++ client/src/components/movies/MoviePreview.tsx
@@ -1,6 +1,6 @@
 const MoviePreview = () => {
     return (
-        <div className="bg-red-400 w-56 h-full mr-1 my-auto rounded-md shrink-0">
+        <div className="bg-red-400 w-56 h-full my-auto rounded-md shrink-0">
             
         </div>
     )
