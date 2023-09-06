9abaef7beb2822dae2c50080d359c430e571e75d
JeffC25 <34695547+JeffC25@users.noreply.github.com>
2023-09-06 10:05:36 -0400

    remove hamburger menu import

--- client/src/components/sidebar/Sidebar.tsx
+++ client/src/components/sidebar/Sidebar.tsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import menuIcon from "../../assets/menu.svg";
 import homeIcon from "../../assets/home.svg";
 import trendingIcon from "../../assets/trending.svg";
 import nowPlayingIcon from "../../assets/nowplaying.svg";
